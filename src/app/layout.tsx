32923a4a51a55c2413563e5c7f33ad9bfdc4208e
janakanupehewage <janakanupehewage02@gmail.com>
2025-03-20 09:59:36 +0530

    new layout changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
+//import { Geist } from "next/font/google";
 import "./globals.css";
 //import StarsCanvas from "@/(components)/main/StarBackground";
 //import Navbar from "@/(components)/main/Navbar";
@@ -8,10 +8,10 @@ import "./globals.css";
 
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
 
 
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable}   bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+        className="bg-[#030014] overflow-y-scroll overflow-x-hidden"
       >
         
         
